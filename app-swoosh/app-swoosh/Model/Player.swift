7af548011d5b64923fee66926955e59f40d0e760
Juan Bernardo Cruz Ortiz <Bernardo@macs-MacBook-Pro.local>
2018-06-06 10:19:45 -0500

    Lesson 09

---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Juan Bernardo Cruz Ortiz on 6/3/18.
+//  Copyright © 2018 Juan Bernardo Cruz Ortiz. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+}
